f7215b6ec68d213a484c851db95073a3ef0e00f3
Jennifer Han <jhan2@andrew.cmu.edu>
2015-12-05 12:30:53 -0500

    simple queries, missing some values that can be added after database is created

--- simple_queries.sql
+++ simple_queries.sql
@@ -1,38 +1,52 @@
 --- SIMPLE QUERIES ---
 
 -- 1. As a driver, I want to sign up with my car.
--- *** Current location needs to be stored differently?
-INSERT INTO Driver
-VALUES ('current_location_value', 'is_available_value', 'user_id_value');
+INSERT INTO Drivers
+VALUES (79.9764, 40.4397, true, 2);
 
 
 -- 2. As an admin, I want to add a new type of car listing into Uber.
-INSERT INTO Car
-VALUES ('manufacturer_value', 'model_value', 'year_value', 'ride_type_value');
+INSERT INTO Cars
+VALUES ('Honda', 'Civic', 2014, 'X');
 
 
 -- 3. As a rider, I want to sign up.
--- *** Need to insert into both User and Rider tables??
-INSERT INTO 
+INSERT INTO Users
+VALUES ('jimmy.fallon@gmail.com', 'iloveponies', 'Jimmy', 'Fallon', '6104290811', 'USA');
+
+INSERT INTO Riders
+VALUES ('some_user_id_value');
 
 
--- 4. As a driver, I want to set myself as available to give rides
-UPDATE Driver
+-- 4. As a driver, I want to set myself as available to give rides.
+UPDATE Drivers
 SET is_available
 WHERE id = 'some_driver_id_value';
 
 
--- 5. As an admin, I want to view ride history on a particular day
+-- 5. As an admin, I want to view ride history on a particular day.
+SELECT *
+FROM Rides
+WHERE start_datetime == 'some_datetime_value';
 
 
 -- 6. As an admin, I want to view the list of all available drivers in the U.S.
--- *** How to denote in the U.S. from current_location
 SELECT *
-FROM Driver
-WHERE (is_available == true) -- AND (current_location ...need to complete here)
+FROM Drivers as d
+INNER JOIN Users as u
+ON d.user_id = u.id
+WHERE (d.is_available == true) AND (u.country == 'USA');
 
 
--- 7. As a rider, I want to be able to pay with different payment options
+-- 7. As a rider, I want to link my PayPal account to my Uber account.
+INSERT INTO PaymentMethods
+VALUES ('Jimmy', 'Fallon', '15289');
+
+INSERT INTO UserPaymentMethods
+VALUES (6, 6, 'Jimmys PayPal', 'date_added_value');
+
+INSERT INTO PayPals
+VALUES (6, 'jimmy.fallon@gmail.com', 'iloveponies');
 
 
 
