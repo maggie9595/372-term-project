86dc2377da2ff346da1844558ae441be7bed5d34
Israel M <purelogiq@gmail.com>
2015-12-05 12:21:29 -0500

    added initial create and initialize

---
+++ create.sql
@@ -0,0 +1,119 @@
+-- Drop dependent tables first
+DROP TABLE IF EXISTS UserPaymentMethods;
+DROP TABLE IF EXISTS CarOwnerships;
+DROP TABLE IF EXISTS Transactions;
+DROP TABLE IF EXISTS Rides;
+DROP TABLE IF EXISTS Cars;
+DROP TABLE IF EXISTS Riders;
+DROP TABLE IF EXISTS Drivers;
+DROP TABLE IF EXISTS Administrators;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS CreditCards;
+DROP TABLE IF EXISTS PayPals;
+DROP TABLE IF EXISTS GoogleWallets;
+DROP TABLE IF EXISTS PaymentMethods;
+
+-- Create the tables
+CREATE TABLE Users (
+  id           SERIAL PRIMARY KEY,
+  email        TEXT NOT NULL UNIQUE,
+  password     TEXT NOT NULL,
+  first_name   TEXT NOT NULL,
+  last_name    TEXT NOT NULL,
+  phone_number TEXT NOT NULL,
+  country      TEXT NOT NULL
+);
+
+CREATE TABLE Riders (
+  id      SERIAL PRIMARY KEY,
+  user_id SERIAL REFERENCES Users (id)
+);
+
+CREATE TABLE Drivers (
+  id                SERIAL PRIMARY KEY,
+  user_id           SERIAL REFERENCES Users (id),
+  current_latitude  FLOAT,
+  current_longitude FLOAT,
+  is_available      BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE Administrators (
+  id      SERIAL PRIMARY KEY,
+  user_id SERIAL REFERENCES Users (id),
+  title   TEXT NOT NULL
+);
+
+CREATE TABLE PaymentMethods (
+  id                 SERIAL PRIMARY KEY,
+  billing_first_name TEXT NOT NULL,
+  billing_last_name  TEXT NOT NULL,
+  billing_zipcode    TEXT NOT NULL
+);
+
+CREATE TABLE CreditCards (
+  id                SERIAL PRIMARY KEY,
+  payment_method_id SERIAL REFERENCES PaymentMethods (id),
+  card_number       TEXT NOT NULL,
+  expiration_month  INT  NOT NULL,
+  expiration_year   INT  NOT NULL,
+  cvv_code          TEXT NOT NULL
+);
+
+CREATE TABLE PayPals (
+  id                SERIAL PRIMARY KEY,
+  payment_method_id SERIAL REFERENCES PaymentMethods (id),
+  paypal_email      TEXT NOT NULL,
+  paypal_password   TEXT NOT NULL
+);
+
+CREATE TABLE GoogleWallets (
+  id                SERIAL PRIMARY KEY,
+  payment_method_id SERIAL REFERENCES PaymentMethods (id),
+  login_email       TEXT NOT NULL,
+  login_password    TEXT NOT NULL
+);
+
+CREATE TABLE UserPaymentMethods (
+  id                      SERIAL PRIMARY KEY,
+  user_id                 SERIAL REFERENCES Users (id),
+  payment_method_id       SERIAL REFERENCES PaymentMethods (id),
+  payment_method_nickname TEXT      NOT NULL,
+  date_added              TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Cars (
+  id           SERIAL PRIMARY KEY,
+  manufacturer TEXT NOT NULL,
+  model        TEXT NOT NULL,
+  year         INT  NOT NULL,
+  ride_type    TEXT NOT NULL
+);
+
+CREATE TABLE CarOwnerships (
+  id                  SERIAL PRIMARY KEY,
+  driver_id           SERIAL REFERENCES Drivers (id),
+  car_id              SERIAL REFERENCES Cars (id),
+  license_plate       TEXT NOT NULL,
+  car_mileage         INT  NOT NULL,
+  drivers_license_num TEXT NOT NULL
+);
+
+CREATE TABLE Rides (
+  id              SERIAL PRIMARY KEY,
+  rider_id        SERIAL REFERENCES Riders (id),
+  driver_id       SERIAL REFERENCES Drivers (id),
+  start_latitude  FLOAT     NOT NULL,
+  start_longitude FLOAT     NOT NULL,
+  destination     TEXT      NOT NULL,
+  mileage         INT       NOT NULL,
+  start_datetime  TIMESTAMP NOT NULL,
+  end_datetime    TIMESTAMP NOT NULL
+);
+
+CREATE TABLE Transactions (
+  id                SERIAL PRIMARY KEY,
+  ride_id           SERIAL REFERENCES Rides (id),
+  payment_method_id SERIAL REFERENCES PaymentMethods (id),
+  price             NUMERIC(10, 2) NOT NULL,
+  datetime_paid     TIMESTAMP
+);
---
+++ initialize.sql
@@ -0,0 +1,68 @@
+-- CSV Files located in the csv_data folder in the same directory as this script.
+
+-- Import Users
+COPY Users(email, "password", first_name, last_name, phone_number, country)
+FROM 'csv_data/users.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import Riders
+COPY Riders(user_id)
+FROM 'csv_data/riders.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import Driver
+COPY Driver(user_id, current_latitude, current_longitude, is_available)
+FROM 'csv_data/drivers.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import Administrators
+COPY Administrators(user_id, title)
+FROM 'csv_data/administrators.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import PaymentMethods
+COPY PaymentMethods(billing_first_name, billing_last_name, billing_zipcode)
+FROM 'csv_data/paymentmethods.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import CreditCards
+COPY CreditCards(id, payment_method_id, card_number, expiration_month,
+                 expiration_year, cvv_code)
+FROM 'csv_data/creditcards.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import PayPals
+COPY PayPals(payment_method_id, paypal_email, paypal_password)
+FROM 'csv_data/paypals.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import GoogleWallets
+COPY GoogleWallets(payment_method_id, login_email, login_password)
+FROM 'csv_data/googlewallets.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import UserPaymentMethods
+COPY UserPaymentMethods(user_id, payment_method_id, payment_method_nicknames, date_added)
+FROM 'csv_data/userpaymentmethods.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import Cars
+COPY Cars(manufacturer, model, "year", ride_type)
+FROM 'csv_data/cars.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import CarOwnwerships
+COPY CarOwnerships(driver_id, car_id, license_plate, car_mileage, driver_license_num)
+FROM 'csv_data/carownerships.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import Rides
+COPY Rides(rider_id, driver_id, start_latitude, start_longitude,
+           destination, mileage, start_datetime, end_datetime)
+FROM 'csv_data/rides.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
+
+-- Import Transactions
+COPY Transactions(ride_id, payment_method_id, price, datetime_paid)
+FROM 'csv_data/transactions.csv'
+WITH QUOTE '"' DELIMITER ',' CSV;
